27d9b4f85450d09ea9d0f89b210895aefb0ae1ed
sakuramilk <c.sakuramilk@gmail.com>
2012-09-09 01:26:59 +0900

    update fingerprint LG1 - > LG6

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2om TARGET_DEVICE=d2dcm BUILD_FINGERPRINT="samsung/d2om/d2dcm:4.0.4/IMM76D/SC06DOMALG1:user/release-keys" PRIVATE_BUILD_DESC="d2om-user 4.0.4 IMM76D SC06DOMALG1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2om TARGET_DEVICE=d2dcm BUILD_FINGERPRINT="samsung/d2om/d2dcm:4.0.4/IMM76D/SC06DOMALG6:user/release-keys" PRIVATE_BUILD_DESC="d2om-user 4.0.4 IMM76D SC06DOMALG6 release-keys"
 
 PRODUCT_NAME := cm_d2dcm
 PRODUCT_DEVICE := d2dcm
