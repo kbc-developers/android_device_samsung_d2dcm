be9dfd19f6370eaf7c51b01998dcdfed6553b955
sakuramilk <c.sakuramilk@gmail.com>
2013-01-14 18:16:33 +0900

    merge from device/samsung/d2att

--- device.mk
+++ device.mk
@@ -17,8 +17,6 @@
 ## (2) Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/d2dcm/d2dcm-vendor.mk)
 
-
-
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/d2dcm/overlay
 ## common overlays
@@ -26,5 +24,3 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/d2-common/overlay-gsm
 
 # Inherit from d2-common
 $(call inherit-product, device/samsung/d2-common/d2-common.mk)
-
-$(call inherit-product-if-exists, vendor/samsung/d2dcm/d2dcm-vendor.mk)
