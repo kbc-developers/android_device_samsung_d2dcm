f2314ba0388d9db7b9aac7b7d8934f420ef48df6
sakuramilk <c.sakuramilk@gmail.com>
2014-01-15 22:30:42 +0900

    Revert "add recovery-preprocess"
    This reverts commit ae00f6403e78306ee77a798748f60b6617abf67c.

--- full_d2dcm.mk
+++ full_d2dcm.mk
@@ -31,8 +31,7 @@ PRODUCT_COPY_FILES += \
     device/samsung/d2dcm/ramdisk/init.carrier.rc:root/init.carrier.rc \
     device/samsung/d2dcm/ramdisk/ueventd.qcom.rc:root/ueventd.qcom.rc \
     device/samsung/d2dcm/ramdisk/init.qcom.early_boot.sh:root/init.qcom.early_boot.sh \
-    device/samsung/d2dcm/ramdisk/init.prop.sh:root/init.prop.sh \
-    device/samsung/d2dcm/ramdisk/sbin/recovery-prepprocess:recovery/root/sbin/recovery-prepprocess
+    device/samsung/d2dcm/ramdisk/init.prop.sh:root/init.prop.sh
 
 # Default Locale
 PRODUCT_PROPERTY_OVERRIDES += \
