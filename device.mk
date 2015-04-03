4e1b28ff60ef593c172664ceddd54498b40c7dae
sakuramilk <c.sakuramilk@gmail.com>
2015-04-03 16:46:16 +0900

    d2dcm: add common overlay

--- device.mk
+++ device.mk
@@ -19,6 +19,8 @@ $(call inherit-product-if-exists, vendor/samsung/d2dcm/d2dcm-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/d2dcm/overlay
+## common overlays
+DEVICE_PACKAGE_OVERLAYS += device/samsung/d2-common/overlay-gsm
 
 # Inherit from d2-common
 $(call inherit-product, device/samsung/d2-common/d2-common.mk)
