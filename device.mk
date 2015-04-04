3328d3a606b23f9e23308c19cc397940beb54ab0
sakuramilk <c.sakuramilk@gmail.com>
2015-04-04 23:33:36 +0900

    d2dcm: change ril blobs
    Change-Id: I7eab763e6b703191567a00c7a70025f8f568085f

--- device.mk
+++ device.mk
@@ -15,12 +15,11 @@
 #
 
 ## (2) Also get non-open-source specific aspects if available
+$(call inherit-product-if-exists, vendor/samsung/d2gsm/d2gsm-vendor.mk)
 $(call inherit-product-if-exists, vendor/samsung/d2dcm/d2dcm-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/d2dcm/overlay
-## common overlays
-DEVICE_PACKAGE_OVERLAYS += device/samsung/d2-common/overlay-gsm
 
 # Inherit from d2-common
 $(call inherit-product, device/samsung/d2-common/d2-common.mk)
